c23994b7f28ea052f0294a93e214c4a7703c2649
AndreaMaier <33418864+AndreaMaier@users.noreply.github.com>
2018-05-29 10:19:55 +0200

    final Code
    Distance Matrix & Clustering

--- ClusteringII_D_Clustering.py
+++ ClusteringII_D_Clustering.py
@@ -10,24 +10,37 @@ import pandas as pd
 import numpy as np
 from geopy.distance import vincenty
 import math
+from sklearn.cluster import DBSCAN
+from scipy.spatial.distance import pdist, squareform
+from math import *
 
 data = preparedData.data_Transformation()
+data_lonlat = data.drop(['name'], axis=1)
 
-def distance_matrix():
-    length = max(data.index)+1
-    distance_matrix = np.empty([length, length])
-    for i in data.index:
-        for j in data.index:
-            if i in data.index and j in data.index:
-                p1_long = data['longitude'][i]
-                p1_lat = data['latitude'][i]
-                p2_long = data['longitude'][j]
-                p2_lat = data['latitude'][j]
-                distance_long = (p1_long - p2_long) * 71500
-                distance_lat = (p1_lat - p2_lat) * 111300
-                distance_matrix[i][j] = math.sqrt(math.pow(distance_long,2)+math.pow(distance_lat,2))
-    return distance_matrix
+def haversine(lonlat1, lonlat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lat1, lon1 = lonlat1
+    lat2, lon2 = lonlat2
+    lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
 
-distance_matrix = distance_matrix()
-            
-#TO DO Perform Clustering
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+    c = 2 * math.asin(math.sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+distance_matrix = squareform(pdist(data_lonlat, (lambda u,v: haversine(u,v))))
+print(distance_matrix)
+
+db = DBSCAN(eps=0.35, min_samples=2, metric="precomputed")
+db.fit_predict(distance_matrix)
+print(db.labels_)
+print(len([i for i in db.labels_ if i == -1]))
+data['cluster']=db.labels_
+print(data)
